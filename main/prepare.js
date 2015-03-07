c1ea8e1401eb9ac5e7937aaddfb4c011b3c55775
Mmis1000 <mmis1000@yahoo.com.tw>
2015-03-08 04:45:44 +0800

    fix resizeable and add start script for *nix

--- main/prepare.js
+++ main/prepare.js
@@ -5,4 +5,6 @@ win.x = 0;
 win.y = 0;
 
 win.setMaximumSize(screen.width, screen.height);
+win.setMinimumSize(screen.width, screen.height);
+win.setResizable(false);
 win.resizeTo(screen.width, screen.height);
